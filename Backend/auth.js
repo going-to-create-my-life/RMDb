4700f15bd06ba04972c0c9dbf6d9a2b7fbb5d204
going-to-create-my-life <148004949+going-to-create-my-life@users.noreply.github.com>
2023-12-30 12:54:44 +0530

    Update auth.js

--- Backend/auth.js
+++ Backend/auth.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database("/home/sourav/Documents/RMdb/Backend/index.db", sqlite3.OPEN_READWRITE,(err)=>{
+const db = new sqlite3.Database("/opt/render/project/src/Backend/index.db", sqlite3.OPEN_READWRITE,(err)=>{
     if(err) return console.error(err.message);
 });
 const jwt = require('jsonwebtoken');
