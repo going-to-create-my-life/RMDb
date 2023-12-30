091b1a465c0adb4a6f0d4c32c677b1154d04591a
going-to-create-my-life <148004949+going-to-create-my-life@users.noreply.github.com>
2023-12-30 12:53:41 +0530

    Add files via upload

--- Backend/auth.js
+++ Backend/auth.js
@@ -1,6 +1,25 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database("/home/sourav/Documents/RMdb/Backend/index.db", sqlite3.OPEN_READWRITE,(err)=>{
+    if(err) return console.error(err.message);
+});
 const jwt = require('jsonwebtoken');
+function getAllPromise(query, params) {
+    return new Promise((resolve, reject) => {
+  
+        db.all(query, params, (err, rows) => {
+  
+            if(err) {
+              console.log(err);
+              res.redirect('/dashboard')
+            }
+  
+            // "return" the result when the action finish
+            resolve(rows);
+        })
+    })
+  }
 
-const auth = (req,res,next) => {
+const auth = async (req,res,next) => {
     let cookie = req.cookies.jwt;
     if(cookie){
         var deco =  jwt.verify(cookie,"RMDb", function(err, decode) {
@@ -16,10 +35,22 @@ const auth = (req,res,next) => {
           });
     }
     else{
+        let netflix = await getAllPromise(`SELECT * FROM allmovies WHERE ott LIKE '%Net%'`);
+        let prime = await getAllPromise(`SELECT * FROM allmovies WHERE ott LIKE '%Amazon%'`);
+        let hotstar = await getAllPromise(`SELECT * FROM allmovies WHERE ott LIKE '%Hotstar%'`);
+        let theature = await getAllPromise(`SELECT * FROM allmovies WHERE ott LIKE '%Theature%'`);
+        let celebrity = await getAllPromise(`SELECT * FROM allcelebrities`);
+        let movie = await getAllPromise(`SELECT * FROM allmovies`);
         return res.render('index',{
-            error : "Sorry you have to login to check RMDb",
+            erro : "Sorry you have to login to check RMDb",
             Login: `<li class="loginLink"><a href="#">LOG In</a></li>`,
-            Signup : `<li class="btn"><a href="/auth/google">Signup via Google</a></li>`
+            Signup : `<li class="btn"><a href="/auth/google">Signup via Google</a></li>`,
+            netflix: netflix,
+            prime: prime,
+            hotstar: hotstar,
+            theature: theature,
+            celebrity: celebrity,
+            movie: movie
         })
     }
 };
